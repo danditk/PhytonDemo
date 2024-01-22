3e3fe5c17fa4038cd407b4d40bea82801961b2c3
Darek <47521520+danditk@users.noreply.github.com>
2024-01-22 21:40:24 +0100

    Examples of inputs

--- PyCharm/L1/L1.py
+++ PyCharm/L1/L1.py
@@ -7,3 +7,16 @@ print()
 print((2 + 2) * 2)
 
 #TODO Plan
+
+a = int(input("Podaj liczbę 1 "))
+b = int(input("Podaj liczbę 2 "))
+
+"""
+alternatywa:
+a = input("Podaj liczbę 1 ")
+b = input("Podaj liczbę 1 ")
+
+print(int(a) == int(b)) / true - false - bool
+"""
+
+print(a + b)
